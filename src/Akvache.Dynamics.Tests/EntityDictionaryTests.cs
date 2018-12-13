5dd938846c998153230f31341c05e65f87ff93b3
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-12-13 16:21:07 +0100

    Starting adding serializers and deserializers

---
+++ src/Akvache.Dynamics.Tests/EntityDictionaryTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Akvache.Dynamics.Tests
+{
+    using System;
+    using System.IO;
+    using Akvache.Dynamics.Implementations;
+    using Microsoft.Xrm.Sdk;
+    using Xunit;
+
+    public class EntityDictionaryTests
+    {
+        #region Public Methods
+
+        [Fact]
+        public void EntityDictionary_Can_Store_And_Retrieve_Value()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var entity = new Entity("test", id);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"{nameof(EntityDictionaryTests)}.db");
+
+            // Act
+            var dictionary = new PersistentDictionary<string, Entity>(path)
+            {
+                ["test"] = entity
+            };
+
+            var result = dictionary["test"];
+
+            // Assert
+        }
+
+        #endregion Public Methods
+    }
+}
