9d5bbb263cb9083d2a3e123cd01b0fb32cd5b03a
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-12-14 11:34:29 +0100

    Extracting XrmContractResolver class

--- src/Xrm.Json.Serialization/EntityConverter.cs
+++ src/Xrm.Json.Serialization/EntityConverter.cs
@@ -1,30 +1,11 @@
 ﻿namespace Xrm.Json.Serialization
 {
     using System;
-    using System.Collections.Generic;
     using Microsoft.Xrm.Sdk;
     using Newtonsoft.Json;
-    using Newtonsoft.Json.Serialization;
 
     public class EntityConverter : JsonConverter
     {
-        #region Public Constructors
-
-        public EntityConverter()
-        {
-            //JsonConvert.DefaultSettings = () => new JsonSerializerSettings
-            //{
-            //    Converters = new List<JsonConverter>
-            //    {
-            //        new EntityReferenceConverter(),
-            //        new MoneyConverter(),
-            //        new OptionSetConvertor()
-            //    }
-            //};
-        }
-
-        #endregion Public Constructors
-
         #region Public Methods
 
         public override bool CanConvert(Type objectType) =>
@@ -32,20 +13,22 @@
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
+            serializer.ContractResolver = new XrmContractResolver();
+
             throw new NotImplementedException();
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            serializer.ContractResolver = new EntityContactResolver();
+            serializer.ContractResolver = new XrmContractResolver();
 
             var entity = value as Entity;
 
             writer.WriteStartObject();
             writer.WritePropertyName("_reference");
             writer.WriteValue($"{entity?.LogicalName}:{entity?.Id.ToString()}");
-            
-            foreach(var attribute in entity?.Attributes)
+
+            foreach (var attribute in entity?.Attributes)
             {
                 writer.WritePropertyName(attribute.Key);
                 serializer.Serialize(writer, attribute.Value);
@@ -55,39 +38,5 @@
         }
 
         #endregion Public Methods
-
-        #region Private Classes
-
-        private class EntityContactResolver : DefaultContractResolver
-        {
-            #region Private Fields
-
-            private readonly Dictionary<Type, JsonConverter> converters;
-
-            #endregion Private Fields
-
-            #region Public Constructors
-
-            public EntityContactResolver()
-            {
-                converters = new Dictionary<Type, JsonConverter>()
-                {
-                    { typeof(EntityReference), new EntityReferenceConverter() },
-                    { typeof(Money), new MoneyConverter() },
-                    { typeof(OptionSetValue), new OptionSetConvertor()}
-                };
-            }
-
-            #endregion Public Constructors
-
-            #region Protected Methods
-
-            protected override JsonConverter ResolveContractConverter(Type objectType) => 
-                converters[objectType];
-
-            #endregion Protected Methods
-        }
-
-        #endregion Private Classes
     }
 }
---
+++ src/Xrm.Json.Serialization/XrmContractResolver.cs
@@ -0,0 +1,38 @@
+﻿namespace Xrm.Json.Serialization
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Xrm.Sdk;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+
+    internal class XrmContractResolver : DefaultContractResolver
+    {
+        #region Private Fields
+
+        private readonly Dictionary<Type, JsonConverter> converters;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public XrmContractResolver()
+        {
+            converters = new Dictionary<Type, JsonConverter>()
+                {
+                    { typeof(EntityReference), new EntityReferenceConverter() },
+                    { typeof(Money), new MoneyConverter() },
+                    { typeof(OptionSetValue), new OptionSetConvertor()}
+                };
+        }
+
+        #endregion Public Constructors
+
+        #region Protected Methods
+
+        protected override JsonConverter ResolveContractConverter(Type objectType) =>
+            converters[objectType];
+
+        #endregion Protected Methods
+    }
+}
