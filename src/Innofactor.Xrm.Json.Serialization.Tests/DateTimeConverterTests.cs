5d593f3232860fc8110cb7be37fb055109727bd6
Alexey Shytikov <alexey.shytikov@cinteros.se>
2019-04-26 08:57:19 +0200

    Adding tests for `DateTimeConverter`

---
+++ src/Innofactor.Xrm.Json.Serialization.Tests/DateTimeConverterTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Innofactor.Xrm.Json.Serialization.Tests
+{
+    using System;
+    using Newtonsoft.Json;
+    using Xunit;
+
+    public class DateTimeConverterTests
+    {
+        #region Public Methods
+
+        [Fact]
+        public void DateTime_Can_Deserialize()
+        {
+            // Arrange
+            var expected = DateTime.UtcNow;
+            var value = $"{{\"_moment\": {expected.Ticks}}}";
+
+            // Act
+            var actual = JsonConvert.DeserializeObject<DateTime>(value, new DateTimeConverter());
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DateTime_Can_Serialize()
+        {
+            // Arrange
+            var value = DateTime.UtcNow;
+            var expected = $"{{\"_moment\": {value.Ticks}}}";
+
+            // Act
+            var actual = JsonConvert.SerializeObject(value, Formatting.None, new DateTimeConverter());
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion Public Methods
+    }
+}
